c81243e042ed0b7d5865e8a5899e0399ff042a0b
lorenzo dicara <dicara97@live.it>
2023-06-26 11:20:15 +0200

    push

---
+++ src/app/models/document.model.ts
@@ -0,0 +1,8 @@
+export interface Document {
+    title: string;
+    optionalValues?: [{
+        name: string;
+        type: string;
+        isRequired: boolean
+    }]
+}
---
+++ src/app/services/document.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+import { Document } from '../models/document.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentService {
+
+  private documentSubject: BehaviorSubject<any> = new BehaviorSubject<void>(undefined);
+  public document: Observable<Document>
+  constructor(private http: HttpClient) {
+    this.document = this.documentSubject.asObservable()
+   }
+
+   public get docValue(): Document {
+    return this.documentSubject.value
+  }
+
+  public addValue(data: any): Document {
+    this.documentSubject.next(data)
+    return this.documentSubject.value
+  }
+
+   addDocument(document: any){
+    return this.http.post<any>(`${environment.api}/document/create`, {document})
+    .pipe(map((data: Document )=> {
+      localStorage.setItem('document', JSON.stringify(data));
+      return data
+    }))
+  }
+}
